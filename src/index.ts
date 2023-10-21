6836a3e1aab3a3f85bd092e2519f1cd8bc85255c
i-shinya <i.shinya0376@gmail.com>
2023-10-21 17:28:28 +0900

    chore adminjsのベースを追加

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,38 @@
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
 import fastify from 'fastify'
-import { healthRoutes } from './router/health'
+import { healthRoutes } from './router/health.js'
+import AdminJS from 'adminjs'
+import AdminJSFastify from '@adminjs/fastify'
 
-const server = fastify()
+const start = async () => {
+  const server = fastify()
+  const admin = new AdminJS({
+    databases: [],
+    rootPath: '/admin',
+  })
 
-// swaggerの設定
-server.register(fastifySwagger)
-server.register(fastifySwaggerUi)
+  await AdminJSFastify.buildRouter(admin, server)
 
-// router
-server.register(healthRoutes, {
-  prefix: '/health',
-})
+  // swaggerの設定
+  server.register(fastifySwagger)
+  server.register(fastifySwaggerUi)
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-  console.log(
-    `swagger ui http://localhost:8080/documentation/static/index.html`,
-  )
-})
+  // router
+  server.register(healthRoutes, {
+    prefix: '/health',
+  })
+
+  server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+    console.log(
+      `swagger ui http://localhost:8080/documentation/static/index.html`,
+    )
+  })
+}
+
+start()
